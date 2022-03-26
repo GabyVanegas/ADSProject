6541be8f71e1eff2c6e11a819bdf0619ac4db901
Gaby <gabriela0vanegas@gmail.com>
2022-03-25 21:12:08 -0600

    Agregar archivos de proyecto.

---
+++ ADSProject/Controllers/MateriaController.cs
@@ -0,0 +1,102 @@
+﻿using ADSProject.Models;
+using ADSProject.Repository;
+using ADSProject.Utils;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSProject.Controllers
+{
+    public class MateriaController : Controller 
+    {
+        private readonly IMateriaRespository materiaRepository;
+
+        public MateriaController(IMateriaRespository materiaRespository)
+        {
+            this.materiaRepository = materiaRespository;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            try
+            {
+                var item = materiaRepository.obtenerMateria();
+
+                return View(item);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Form(int? idmateria, Operaciones operaciones)
+        {
+            try
+            {
+                var materia = new MateriaViewModel();
+
+                if (idmateria.HasValue)
+                {
+                    materia = materiaRepository.obtenerMateriaPorID(idmateria.Value);
+                }
+                // Indica el tipo de operacion que es esta realizando
+                ViewData["Operaciones"] = operaciones;
+
+                return View(materia);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Form(MateriaViewModel materiaViewModel)
+        {
+            try
+            {
+                if (materiaViewModel.idMateria == 0) // En caso de insertar
+                {
+                    materiaRepository.agregarMateria(materiaViewModel);
+                }
+                else // En caso de actualizar
+                {
+                    materiaRepository.actualizarMateria
+                        (materiaViewModel.idMateria, materiaViewModel);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int idmateria)
+        {
+            try
+            {
+                materiaRepository.eliminarMateria(idmateria);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ADSProject/Models/MateriaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ADSProject.Utils;
+using System.ComponentModel.DataAnnotations;
+
+namespace ADSProject.Models
+{
+    public class MateriaViewModel
+    {
+        [Display(Name = "ID")]
+
+        public int idMateria { get; set; }
+
+        [Required(ErrorMessage = Constants.REQUIRED_FIELD)]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "La longitud del campo no debe de ser mayor de 50 caracteres ni menor de 3 caracteres. ")]
+        [Display(Name = "NombreMateria")]
+        public string nombreMateria { get; set; }
+        
+    }
+}
